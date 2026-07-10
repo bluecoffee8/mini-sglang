297529ba2c961d7ea60cb8f85e43d2d62c5b1c42
bluecoffee8 <jasperli2002@gmail.com>
2026-07-10 13:29:45 -0700

    fewer scales

--- benchmark/online/bench_qwen.py
+++ benchmark/online/bench_qwen.py
@@ -52,7 +52,8 @@ async def main():
     random.seed(42)  # reproducibility
     PORT = 1919
     N = 1000
-    SCALES = [0.4, 0.5, 0.6, 0.7, 0.8, 1.6]  # from fast to slow
+    # SCALES = [0.4, 0.5, 0.6, 0.7, 0.8, 1.6]  # from fast to slow
+    SCALES = [0.4, 1.6]
     async with OpenAI(base_url=f"http://127.0.0.1:{PORT}/v1", api_key="dummy") as client:
         MODEL = await get_model_name(client)
         tokenizer = AutoTokenizer.from_pretrained(MODEL)
